f828a5ed2694ff4207feaa741dad8c5fc39a6599
Samir Khanal <gdssamir@gmail.com>
2025-11-14 22:04:42 +0545

    chore: setup initial server

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,24 @@
 import { NestFactory } from '@nestjs/core';
+import { Logger } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  app.setGlobalPrefix('v1');
+
+  const configService = app.get<ConfigService>(ConfigService);
+
+  app.enableCors({
+    origin: configService.get<string>('PUBLIC_WEB_URL'),
+    credentials: true,
+  });
+
+  const port = configService.get<number>('BE_PORT') ?? 7000;
+  await app.listen(port);
+  Logger.log(`Server is running on port ${port}`);
 }
-bootstrap();
+bootstrap().catch((error) => {
+  Logger.error(error);
+  process.exit(1);
+});
