30c8b41ee7cd49265d21adae592e80a44c53d1ba
Samir Khanal <gdssamir@gmail.com>
2025-11-15 10:59:35 +0545

    Setup (#4)
    * chore: update eslint config
    
    * chore: add prisma
    
    * feat: user schema migration
    
    * chore: update run setup
    
    * chore: setup prisma module
    
    * chore: setup initial server
    
    * refact: readme
    
    * fix: prisma error
    
    * chore: setup swagger docs

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,36 @@
 import { NestFactory } from '@nestjs/core';
+import { Logger } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  app.setGlobalPrefix('v1');
+
+  const configService = app.get<ConfigService>(ConfigService);
+
+  app.enableCors({
+    origin: configService.get<string>('PUBLIC_WEB_URL'),
+    credentials: true,
+  });
+
+  const config = new DocumentBuilder()
+    .setTitle('Pasal API Documentation')
+    .setDescription('Backend API documentation')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
+  const port = configService.get<number>('BE_PORT') ?? 7000;
+  await app.listen(port);
+
+  Logger.log(`Server is running on port ${port}`);
+  Logger.log(`Swagger docs available at http://localhost:${port}/docs`);
 }
-bootstrap();
+bootstrap().catch((error) => {
+  Logger.error(error);
+  process.exit(1);
+});
