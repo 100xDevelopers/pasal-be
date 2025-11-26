816afd0f633380f11a89d4c49345966e3978c6b8
Samir Khanal <gdssamir@gmail.com>
2025-11-26 22:36:16 +0545

    chore: add global pipes

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import { NestFactory } from '@nestjs/core';
-import { Logger } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
@@ -14,6 +14,16 @@ async function bootstrap() {
     origin: configService.get<string>('PUBLIC_WEB_URL'),
     credentials: true,
   });
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // remove unwanted values
+      transform: true, // transform to dto objects
+      transformOptions: {
+        enableImplicitConversion: true,
+      },
+    }),
+  );
 
   const config = new DocumentBuilder()
     .setTitle('Pasal API Documentation')
