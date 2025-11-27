218216db90b6a63701a60235fc3bfa944bc64897
Samir Khanal <gdssamir@gmail.com>
2025-11-27 15:58:45 +0545

    fix: docker env issue

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,7 +1,15 @@
-import { defineConfig } from '@prisma/config';
+import { defineConfig, env } from '@prisma/config';
+import dotenv from 'dotenv';
+import { expand } from 'dotenv-expand';
+
+expand(dotenv.config()); // for docker compose vars
+
 export default defineConfig({
   schema: 'prisma/schema.prisma',
+  migrations: {
+    path: 'prisma/migrations',
+  },
   datasource: {
-    url: process.env.DATABASE_URL!,
+    url: env('DATABASE_URL'),
   },
 });
