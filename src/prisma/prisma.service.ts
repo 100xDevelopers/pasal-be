a809fd79cfb8e0790ae8b690fe820dd1dbc3f7fe
Samir Khanal <gdssamir@gmail.com>
2025-11-27 15:26:10 +0545

    refact: migrate prismaService to v7

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,11 +1,19 @@
 import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
+import { ConfigService } from '@nestjs/config';
+import { PrismaPg } from '@prisma/adapter-pg';
 
 @Injectable()
 export class PrismaService
   extends PrismaClient
   implements OnModuleInit, OnModuleDestroy
 {
+  constructor(configService: ConfigService) {
+    const connectionString = configService.get<string>('DATABASE_URL');
+    const adapter = new PrismaPg({ connectionString });
+    super({ adapter });
+  }
+
   async onModuleInit() {
     await this.$connect();
   }
