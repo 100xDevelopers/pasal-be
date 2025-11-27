2a5f79b1f788950f55fc5bc28209d5e76d1b192d
Samir Khanal <gdssamir@gmail.com>
2025-11-27 17:59:59 +0545

    feat: add product schema

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,13 +1,14 @@
-import 'dotenv/config';
-import { defineConfig, env } from 'prisma/config';
+import { defineConfig, env } from '@prisma/config';
+import dotenv from 'dotenv';
+import { expand } from 'dotenv-expand';
+
+expand(dotenv.config()); // for docker compose vars
 
 export default defineConfig({
   schema: 'prisma/schema.prisma',
-
   migrations: {
     path: 'prisma/migrations',
   },
-
   datasource: {
     url: env('DATABASE_URL'),
   },
